d0504d0253a1221d969aa6d47568745ed4da610e
Oliver Fleckenstein <oliverfl@live.dk>
2022-02-07 17:37:57 +0100

    algorithms added

--- Algorithms/Power.cs
+++ Algorithms/Power.cs
@@ -15,4 +15,10 @@ public class PowerSolution
         }
         return result;
     }
+
+    // https://leetcode.com/problems/power-of-two/
+    public bool IsPowerOfTwo(int n)
+    {
+        return n > 0 && (n & (n - 1)) == 0;
+    }
 }
